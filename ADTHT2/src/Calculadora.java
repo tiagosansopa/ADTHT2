ce8f21c4d24d6a22df333f18389184701f6439fe
Tiago <Tiago@172.20.9.244>
2015-07-28 12:40:12 -0600

    Ya Actualice todo. La clase calculadora crea todo ahora. El main solo lee el archivo.

--- ADTHT2/src/Calculadora.java
+++ ADTHT2/src/Calculadora.java
@@ -1,9 +1,72 @@
 
-public class Calculadora {
+public class Calculadora 
+{
+    Pila miPila = new Pila();
+    int contadorOperadores = 0;
+    
+    public Calculadora() 
+    {
 
-	public Calculadora() {
-		// TODO Auto-generated constructor stub
-	}
+    }
 
+    //Llenar el Stack
+    public void recolectarOperadores(String linea)
+    {
+        for(int i = (linea.length()-1); i>=0; i--)
+        {
+                if(linea.charAt(i) != ' ') 
+                {
+                        miPila.push(Character.toString(linea.charAt(i)));
+                        contadorOperadores++;
+                }
+        }
+    }
+    
+    public int realizarCalculos()
+    {
+        //Sacar el Stack y operar
+        String temporal1, temporal2, temporal3, envio;
+        int resultado = 0;
+        
+        for(int i = 0; i <= ((contadorOperadores-1)/2); i++)
+        {
+            temporal1 = miPila.pop(); //Resultado anterior
+            temporal2 = miPila.pop(); //nuevo numero
+            temporal3 = miPila.pop(); //operador
+
+            if( temporal3.equals("+"))
+            {
+                    resultado = Math.addExact(Integer.parseInt(temporal1), Integer.parseInt(temporal2));
+                    //System.out.println("La suma de los numeros es " + resultado);
+                    envio = Integer.toString(resultado);
+                    miPila.push(envio);
+            }
+
+            if( temporal3.equals("/"))
+            {
+                    resultado = Math.floorDiv(Integer.parseInt(temporal1), Integer.parseInt(temporal2));
+                    //System.out.println("La division de los numeros es " + resultado);
+                    envio = Integer.toString(resultado);
+                    miPila.push(envio);
+            }
+
+            if( temporal3.equals("*"))
+            {
+                    resultado = Math.multiplyExact(Integer.parseInt(temporal1), Integer.parseInt(temporal2));
+                    //System.out.println("La multiplicacion de los numeros es " + resultado);
+                    envio = Integer.toString(resultado);
+                    miPila.push(envio);
+            }
+
+            if( temporal3.equals("-"))
+            {
+                    resultado = Math.subtractExact(Integer.parseInt(temporal1), Integer.parseInt(temporal2));
+                    //System.out.println("La division de los numeros es " + resultado);
+                    envio = Integer.toString(resultado);
+                    miPila.push(envio);
+            }
+        }
+        return resultado;
+    }
 }
 
